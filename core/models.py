e0c853a8f15bbcf4733ac52c045fe84715219e7c
MohammadJavad14 <mrph14@gmail.com>
2022-06-11 01:55:40 +0430

    signup page

--- core/models.py
+++ core/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Profile(models.Model):
-    user = pass
-    id_user = pass
-    bio = pass
-    profile_img = pass
-    location = pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True, null=True)
+    profile_img = models.ImageField(
+        upload_to='profile_images', default='blank_profile.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
